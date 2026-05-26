af5041b2e006ea1f3b0c626ddcc0dbe866910759
Mateusz <52793544+Gubeee@users.noreply.github.com>
2026-05-26 18:47:09 +0200

    Add files via upload
    Utworzono proste menu

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Prosty kalkulator Python\n" \
+"Wybierz opcje:\n" \
+"1. Dodawanie\n" \
+"2. Odejmowanie\n" \
+"3. Mnozenie\n" \
+"4. Dzielenie\n" \
+"0. Wyjscie\n\n")
+
+option = input("Wybierz opcje: ")
