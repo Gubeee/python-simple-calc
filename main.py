7002faad0ff21363de541691931cb66d25dbefa2
Mateusz <52793544+Gubeee@users.noreply.github.com>
2026-05-26 18:59:40 +0200

    Add files via upload
    Zaimplementowano dodawanie i odejmowanie

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 print ("Prosty kalkulator Python\n" \
 "Wybierz opcje:\n" \
 "1. Dodawanie\n" \
@@ -6,4 +8,17 @@ print ("Prosty kalkulator Python\n" \
 "4. Dzielenie\n" \
 "0. Wyjscie\n\n")
 
-option = input("Wybierz opcje: ")
+option = int(input("Wybierz opcje: "))
+
+if option == 1:
+	a = int(input("Podaj pierwsza liczbe: "))
+	b = int(input("Podaj druga liczbe: "))
+	print("\nWynik: ")
+	print (a + b)
+	exit()
+elif option == 2:
+	a = int(input("Podaj pierwsza liczbe: "))
+	b = int(input("Podaj druga liczbe: "))
+	print("\nWynik: ")
+	print (a - b)
+	exit()
